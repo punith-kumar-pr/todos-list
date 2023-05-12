23f4298b3ef2c43317e1b278998b98dac1d0999b
Punith Kumar P R <94525391+punith-kumar-pr@users.noreply.github.com>
2023-05-12 20:59:24 +0530

    Added V1

---
+++ src/MyComponents/About.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className='text-center'>
+            I'm Punith Kumar P R
+            <p>Hello</p>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://github.com"><i class="fab fa-github"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://linkedin.com"><i class="fab fa-linkedin"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
